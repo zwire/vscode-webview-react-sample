be86a05c0029cce17e7f53a8e3ddc422867f8f01
Takumi Yamashita <64026753+husty530@users.noreply.github.com>
2024-03-20 12:13:15 +0900

    *

--- README.md
+++ README.md
@@ -26,3 +26,6 @@ Once the sample is open inside VS Code you can run the extension by doing the fo
 
 1. Press `F5` to open a new Extension Development Host window
 2. Inside the host window, open the command palette (`Ctrl+Shift+P` or `Cmd+Shift+P` on Mac) and type `webview sample: Show`
+
+For mode information, see [here](https://github.com/microsoft/vscode-webview-ui-toolkit-samples/tree/main/frameworks/component-gallery-react).
+
--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -12,13 +12,37 @@ import { TagDemo } from "./demos/TagDemo";
 import { TextAreaDemo } from "./demos/TextAreaDemo";
 import { TextFieldDemo } from "./demos/TextFieldDemo";
 import { vscode } from "./utilities/vscode";
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+} from 'reactflow';
+
+import 'reactflow/dist/style.css';
 import "./App.css";
 import "./codicon.css";
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 function App() {
   const button_handle = (name: string) => {
     vscode.postMessage({ name, type: "button_click" });
   };
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
   return (
     <main>
       <h1>Webview UI Toolkit React Component Gallery</h1>
@@ -47,6 +71,19 @@ function App() {
         <TextAreaDemo></TextAreaDemo>
         <TextFieldDemo></TextFieldDemo>
       </section>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+        >
+          <Controls />
+          <MiniMap />
+          <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        </ReactFlow>
+      </div>
     </main>
   );
 }
